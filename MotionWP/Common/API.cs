77060fe96806e0a4496da110f8456988f580e1f8
Luis Fonseca <miguelluisfonseca@gmail.com>
2013-11-21 17:44:13 +0000

    Initial base solution with 2 assemblies with api already implemented.

---
+++ MotionWP/Common/API.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace Common
+{
+    public class API
+    {
+        public enum Actions
+        {
+            CheckIfSomeoneIsPresent,
+            GetLastTimestamp
+        }
+
+        private static Uri getUrlFromAction(Actions action, params object[] parameters)
+        {
+            switch (action)
+            {
+                case Actions.CheckIfSomeoneIsPresent:
+                    return new Uri(string.Format("http://nuieee.fe.up.pt/motion/CheckIfSomeoneIsPresent.php", parameters));
+                case Actions.GetLastTimestamp:
+                    return new Uri(string.Format("http://nuieee.fe.up.pt/motion/GetLastTimestamp.php", parameters));
+            }
+            return null;
+        }
+
+        public static async Task<T> GetAsync<T>(Actions action, params object[] parameters)
+        {
+            Uri uri = getUrlFromAction(action, parameters);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.CreateHttp(uri);
+            //HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync();
+            request.AllowReadStreamBuffering = true;
+            var task = Task<WebResponse>.Factory.FromAsync(
+                new Func<AsyncCallback, object, IAsyncResult>(request.BeginGetResponse),
+                new Func<IAsyncResult, WebResponse>(request.EndGetResponse), null);
+            var response = await task;
+
+            using (Stream streamResponse = response.GetResponseStream())
+            using (StreamReader streamRead = new StreamReader(streamResponse, Encoding.UTF8))
+            {
+                var responseString = streamRead.ReadToEnd();
+
+                DataContractJsonSerializer jsonSerializer = new DataContractJsonSerializer(typeof(T));
+                return (T)jsonSerializer.ReadObject(new MemoryStream(Encoding.Unicode.GetBytes(responseString)));
+            }
+        }
+    }
+}
---
+++ MotionWP/MotionWP/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using MotionWP.Resources;
+using Common;
+using Common.Json;
+
+namespace MotionWP
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Sample code to localize the ApplicationBar
+            //BuildLocalizedApplicationBar();
+            Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            try { 
+                var someonePresent = await API.GetAsync<SomeonePresent>(API.Actions.CheckIfSomeoneIsPresent);
+
+                if (someonePresent.Result.HasValue)
+                {
+                    tbResult.Text = someonePresent.Result.Value ? "yes" : "no";
+                }
+                else
+                {
+                    tbResult.Text = "internal server error";
+                }
+            }
+            catch {
+                tbResult.Text = "web error";
+            }
+        }
+
+        // Sample code for building a localized ApplicationBar
+        //private void BuildLocalizedApplicationBar()
+        //{
+        //    // Set the page's ApplicationBar to a new instance of ApplicationBar.
+        //    ApplicationBar = new ApplicationBar();
+
+        //    // Create a new button and set the text value to the localized string from AppResources.
+        //    ApplicationBarIconButton appBarButton = new ApplicationBarIconButton(new Uri("/Assets/AppBar/appbar.add.rest.png", UriKind.Relative));
+        //    appBarButton.Text = AppResources.AppBarButtonText;
+        //    ApplicationBar.Buttons.Add(appBarButton);
+
+        //    // Create a new menu item with the localized string from AppResources.
+        //    ApplicationBarMenuItem appBarMenuItem = new ApplicationBarMenuItem(AppResources.AppBarMenuItemText);
+        //    ApplicationBar.MenuItems.Add(appBarMenuItem);
+        //}
+    }
+}
